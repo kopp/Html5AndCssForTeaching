a23fd3ff7ade1de9bcd80401f246e5c700ad7c41
Michael Kopp <kopp.michael@yahoo.de>
2019-11-18 12:04:16 +0100

    add python server which does not cache

---
+++ 00_webserver_starten.bat
@@ -0,0 +1,2 @@
+
+python server_without_cache.py
---
+++ server_without_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Start a python web server which marks all pages as not to be cached.
+# See https://stackoverflow.com/a/28708920/2165903
+
+import http.server
+
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=MyHTTPRequestHandler)
